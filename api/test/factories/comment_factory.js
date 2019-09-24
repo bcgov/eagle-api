34847d701dfb2dc1603714d888fd21b4f2a7e45c
Unknown <juliansubda@gmail.com>
2019-09-24 09:24:43 -0700

    Comment Period scaffolding

--- api/test/factories/comment_factory.js
+++ api/test/factories/comment_factory.js
@@ -1,21 +1,45 @@
-//TODO: revise for EPIC
 const factory = require('factory-girl').factory;
 const Comment = require('../../helpers/models/comment');
+const moment = require('moment');
 
-factory.define('comment', Comment, buildOptions => {
+const eaoStatuses = [
+  "Deferred"
+  , "Published"
+  , "Rejected"
+  , "Unvetted"
+];
+
+factory.define('comment', Comment, function (buildOptions) {
+  let dateAdded = moment(faker.date.past(10, new Date()));
+  let datePosted = dateAdded.clone().subtract(faker.random.number(7), 'days');
+  let dateUpdated = datePosted.clone().subtract(faker.random.number(7), 'days');
+
   let attrs = {
-    code: factory.seq('Comment.code', (n) => `comment-code-${n}`),
-    comment: factory.chance('sentence'),
-    name: factory.chance('name'),
-    isDeleted: false,
-    tags: [
-      ['public'], ['sysadmin']
-    ], 
-  }
-  if (buildOptions.public) { 
-    attrs.tags = [['public'], ['sysadmin']];
-  } else if (buildOptions.public === false) {
-    attrs.tags = [['sysadmin']];
+      author              : faker.fake("{{name.firstName}} {{name.lastName}}")
+    , comment             : faker.lorem.paragraph()
+    , dateAdded           : dateAdded
+    , datePosted          : datePosted
+    , dateUpdated         : dateUpdated
+    , documents           : require('mongoose').Types.ObjectId()
+    , eaoNotes            : faker.lorem.paragraph()
+    , eaoStatus           : faker.random.arrayElement(eaoStatuses)
+    , isAnonymous         : faker.random.boolean()
+    , location            : faker.random.number(200) + "km " + faker.random.arrayElement(["", "N", "S"]) + faker.random.arrayElement(["", "E", "W"]) + " of " + faker.address.city()
+    , period              : require('mongoose').Types.ObjectId()
+    , proponentNotes      : faker.lorem.paragraph()
+    , proponentStatus     : ""
+    , publishedNotes      : faker.lorem.paragraph()
+    , rejectedNotes       : faker.lorem.paragraph()
+    , rejectedReason      : ""
+    , valuedComponents    : [require('mongoose').Types.ObjectId(),require('mongoose').Types.ObjectId(), require('mongoose').Types.ObjectId()]
+
+    // Number auto-incremented.  Do not set manually.
+    , commentId           : factory.seq('Comment.code', (n) => '${n}')
+
+    // Permissions
+    , write               : '["project-system-admin"]'
+    , read                : '["project-system-admin"]'
+    , delete              : '["project-system-admin"]'
   }
   return attrs;
 });
