84aca4be29fdfb99c8eb5703c172ec359ea0392d
Unknown <juliansubda@gmail.com>
2019-10-10 11:19:53 -0700

    Model coverage & test cleanup

--- api/test/detect_model_changes.test.js
+++ api/test/detect_model_changes.test.js
@@ -1,47 +1,97 @@
 'use strict';
 const Promise = require("bluebird");
 Promise.longStackTraces();
-const test_helper = require('./test_helper');
-const factory_helper = require('./factories/factory_helper');
-const app = test_helper.app;
-const hashElement = require('folder-hash');
+const { hashElement } = require('folder-hash');
 const _ = require('lodash');
+const fs = require('fs');
 
-const generate_helper = require("./generate_helper");
+const stringify_replacer = null;
+const stringify_space = 2;
 
-// const options = {
-//     folders: { 
-//         include: ['.*', '*_helper.js', 'test_coverage']
-//         , exclude: ['.*', '*_helper.js', 'test_coverage'] },
-//     files: { 
-//         include: ['*.js']
-//         , exclude: ['*_helper.js'] }
-// };
+const options = {
+    folders: { 
+        include: ['./api/helpers/models', './api/test/factories']
+        , exclude: ['.*', 'controllers', 'fixtures'] },
+    files: { 
+        include: ['*.js']
+        , exclude: ['*_helper.js', '*.json'] }
+};
 
-// describe('Catch Model Changes not done in Factories', () => {
-//   const factoriesCoveredByDataGeneration = {};
-//   const modelsCoveredByDataGeneration = {};
+function getLastGoodHashsetFromFile() {
+    return new Promise(resolve => {
+      let filename = './api/test/model_change_watcher_hash.json';
+      let fileContents = "";
+      fs.readFileSync(filename).toString().split('\n').forEach(function (line) { fileContents = fileContents + line; })
+      resolve(fileContents);
+    });   
+  };
 
-//   describe('Check Models & Factories', () => {
-//     test('Generator', done => {
-//       generate_helper.generateAll(usersData).then(generatedData =>{
-//         console.log(((generate_helper.genSettings.generate_consistent_data) ? "Consistent" : "Random") + " data generation " + ((generate_helper.genSettings.save_to_persistent_mongo) ? "saved" : "unsaved"));
-//         //console.log('generatedData: [' + generatedData + ']');
-//         let projects = generatedData[2];
-//         console.log('projects: [' + projects + ']');
+function filterRelevantFolders(apiFolderJsonObj) {
+    let models = "";
+    let factories = "";
+    apiFolderJsonObj.children.forEach(function(apiSubfolder) {
+        switch(apiSubfolder.name) {
+            case "helpers":
+                apiSubfolder.children.forEach(function(helpersSubfolder) {
+                    if ("models" == helpersSubfolder.name) {
+                        models = helpersSubfolder.children;
+                    }
+                });
+              break;
+            case "test":
+                apiSubfolder.children.forEach(function(testSubfolder) {
+                    if ("factories" == testSubfolder.name) {
+                        factories = testSubfolder.children;
+                    }
+                });
+              break;
+            default:
+                // try next
+          }
+    });
+    return JSON.stringify({ models: models, factories: factories }, stringify_replacer, stringify_space);
+}
 
-//         // throw projects
-//         projects.map((project) => {
-//           console.log('Project [id, name]: [' + project._id + ', ' + project.name + ']');
-//           //console.log('        [shortName, dateAdded, dateUpdated]: [' + project.shortName + ', ' + project.dateAdded + ',' + project.dateUpdated + ']');
-//           //console.log('        [projectLead, projectLeadEmail]: [' + project.projectLead + ', ' + project.projectLeadEmail + ']');
-//           expect(project._id).toEqual(jasmine.any(Object));
-//           expect(project.CELeadEmail).toEqual("eao.compliance@gov.bc.ca");
-//           //TODO:: Check the outputted data against the database model
-//           done();
-//         });
-//       });
-//     });
-//   });
 
+/* 
+ * Hello.  Looks like you're here because the models were changed by some of your work.
+ *
+ * The API project is in early days testing-wise, so it is largely uncovered by automation compared to 
+ * the original project PRC from which this was forked.  Since we don't have real tests covering the models,
+ * this test is here to remind you to keep the data generators in sync.
+ * 
+ * Steps to resolve:
+ * 
+ * 1. Review your model changes
+ * 2. Modify the data generators in ./api/test/factories with realistic looking data following the 
+ *    existing data generation patterns.
+ * 3. Run this test locally from project folder ./
+ *      node_modules/.bin/jest ./api/test/detect_model_changes.test.js
+ * 4. Open ./api/test/model_change_watcher_hash.json
+ * 5. Look at the contents, clear them and then copy-paste your output from step 3 into the file
+ * 6. Commit and update your PR
+ *
+ */
+
+describe('Catch Model Changes not done in Factories', () => {
+    describe('Check Models & Factories', () => {
+        test('Detect Changes', done => {
+            getLastGoodHashsetFromFile().then(lastGoodApiFolderHashset => {
+                hashElement('./api', options).then(currentApiFolderHashset => {
+                    
+                    currentApiFolderHashset = filterRelevantFolders(currentApiFolderHashset);
+                    console.log("************** Start snip **************");
+                    console.log(currentApiFolderHashset);
+                    console.log("************** End snip **************");
+                    expect(currentApiFolderHashset.models).toEqual(lastGoodApiFolderHashset.models);
+                    expect(currentApiFolderHashset.factories).toEqual(lastGoodApiFolderHashset.factories);
+                    done();
+                })
+                .catch(error => {
+                    return console.error('hashing failed:', error);
+                });            
+            });
+        });
+    });
 });
+
--- generate.sh
+++ generate.sh
@@ -27,7 +27,7 @@ if ! [[ "${PROJECTS}" =~ $valid_numeric_pattern ]]; then printf "$usage"; exit 1
 if ! [[ "${SEED_MODE}" =~ $valid_seed_mode_pattern ]]; then printf "$usage"; exit 1; fi
 if ! [[ "${DATA_MODE}" =~ $valid_data_mode_pattern ]]; then printf "$usage"; exit 1; fi
 JSON="{\"projects\":\"$PROJECTS\",\"seed_mode\":\"$SEED_MODE\",\"data_mode\":\"$DATA_MODE\"}\n";
-# printf JSON;
+#printf "$JSON\n";
 rm -f /tmp/generate.config;
 printf "$JSON" > /tmp/generate.config;
 node_modules/.bin/jest ./api/test/generate.test.js
