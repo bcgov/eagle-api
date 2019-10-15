4bd663e167f33014e3f0c19c8c2482e41f3bed71
Julian Subda <37681273+ActionAnalytics@users.noreply.github.com>
2019-10-15 12:21:58 -0700

    Simplified includes (#211)

--- install_prerequisites.sh
+++ install_prerequisites.sh
@@ -1,4 +1,2 @@
-curl -LJO https://raw.githubusercontent.com/bcgov/eagle-dev-guides/master/developer_install_helper.sh;
 curl -LJO https://raw.githubusercontent.com/bcgov/eagle-dev-guides/master/developer_install_prerequisites.sh;
-curl -LJO https://raw.githubusercontent.com/bcgov/eagle-dev-guides/master/vscodeextensions.txt;
 source ./developer_install_prerequisites.sh;
