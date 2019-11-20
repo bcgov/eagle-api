c841e4e110431126584c7677f7da4785038d0a9b
Morgan Thompson <morgan.thompson@gov.bc.ca>
2019-11-20 15:16:23 -0800

    add new EAR fields to list model

--- api/helpers/models/list.js
+++ api/helpers/models/list.js
@@ -3,7 +3,10 @@ module.exports = require('../models')('List', {
   type         : { type: String, default: null, index: true },
   item         : { type: String, default:null },
   guid         : { type: String, default:null, index: true },
+  legislation  : { type: Number, default: 0 },
+  listOrder    : { type: Number },
 
   // TODO: Decide who should be able to access.
   read         : [{ type: String, trim: true, default: '["public"]' }],
+  write        : [{ type: String, trim: true, default: '["staff"]' }]
 }, 'epic');
