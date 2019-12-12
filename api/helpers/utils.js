937fb727cba568019d3b9ef2121afb190f1444b7
Alastair Beaumont <a.beaumont11@gmail.com>
2019-12-12 14:25:55 -0800

    Updating utils to work with recent activity

--- api/helpers/utils.js
+++ api/helpers/utils.js
@@ -140,7 +140,8 @@ exports.runDataQuery = async function (modelType, role, query, fields, sortWarmU
             "preserveNullAndEmptyArrays": true
           }
         },
-        (modelType === 'Project' || populateProject) && {
+        // To unpack the legislation data into the project key
+        (modelType === 'Project') && {
           $addFields: {
             "default": {
               $switch: {
@@ -162,7 +163,7 @@ exports.runDataQuery = async function (modelType, role, query, fields, sortWarmU
             }
           }
         },
-        (modelType === 'Project' || populateProject) &&  {
+        (modelType === 'Project') &&  {
           '$addFields': {
             "default.pins": '$pins',
             "default.pinsHistory": '$pinsHistory',
@@ -171,8 +172,47 @@ exports.runDataQuery = async function (modelType, role, query, fields, sortWarmU
             "default.read": '$read'
           }
         },
-        (modelType === 'Project' || populateProject) && {
+        (modelType === 'Project') && {
           "$replaceRoot": { newRoot:  "$default" }
+        },
+
+        //Unpack the default key inside a nested call with project data
+         // To unpack the legislation data into the project key
+         (modelType !== 'Project' && populateProject) && {
+          $addFields: {
+            "project.default": {
+              $switch: {
+                branches: [
+                  { 
+                    case: { $eq: [ "$project.currentLegislationYear", 'legislation_1996' ]},
+                    then: "$project.legislation_1996"
+                  },
+                  {
+                    case: { $eq: [ "$project.currentLegislationYear", 'legislation_2002' ]},
+                    then: "$project.legislation_2002"
+                  },
+                  {
+                    case: { $eq: [ "$project.currentLegislationYear", 'legislation_2018' ]},
+                    then: "$project.legislation_2018"
+                  }
+                ], default: "$project.legislation_2002"
+              }
+            }
+          }
+        },
+        (modelType !== 'Project' && populateProject) &&  {
+          '$addFields': {
+            "project.default.pins": '$project.pins',
+            "project.default.pinsHistory": '$project.pinsHistory',
+            "project.default.pinsRead": '$project.pinsRead',
+            "project.default._id": '$project._id',
+            "project.default.read": '$project.read'
+          }
+        },
+        (modelType !== 'Project' && populateProject) && {
+          "$addFields": {
+            "project": "$project.default"
+          },
         },
         // Add our projection after we have reformatted project
         {
