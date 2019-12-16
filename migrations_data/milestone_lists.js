8aafd3243e69919d2be8e87779d3168cbdc2369f
Alastair Beaumont <a.beaumont11@gmail.com>
2019-12-16 14:20:59 -0800

    Adding in the new milestone lists

---
+++ migrations_data/milestone_lists.js
@@ -0,0 +1,313 @@
+module.exports = [
+  //2002 milestone legislation
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2002,
+    name: "Section 6",
+    listOrder: 0
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2002,
+    name: "Section 7",
+    listOrder: 1
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2002,
+    name: "Section 10",
+    listOrder: 2
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2002,
+    name: "Section 11/14",
+    listOrder: 3
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2002,
+    name: "Section 13/15",
+    listOrder: 4
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2002,
+    name: "AIR",
+    listOrder: 5
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2002,
+    name: "Application Evaluation",
+    listOrder: 6
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2002,
+    name: "Concurrent Permitting",
+    listOrder: 7
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2002,
+    name: "Application Review",
+    listOrder: 8
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2002,
+    name: "Decision",
+    listOrder: 9
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2002,
+    name: "Time Limit Extension",
+    listOrder: 10
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2002,
+    name: "Time Limit Suspension",
+    listOrder: 11
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2002,
+    name: "Withdrawal",
+    listOrder: 12
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2002,
+    name: "Termination",
+    listOrder: 13
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2002,
+    name: "Other",
+    listOrder: 14
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2002,
+    name: "Certificate",
+    listOrder: 15
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2002,
+    name: "Certificate Extension",
+    listOrder: 16
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2002,
+    name: "Substantial Start Decision",
+    listOrder: 17
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2002,
+    name: "Compliance & Enforcement",
+    listOrder: 18
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2002,
+    name: "Post-Decision Materials",
+    listOrder: 19
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2002,
+    name: "Amendment",
+    listOrder: 20
+  },
+  // 2018 milestone legislation
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2018,
+    name: "Project Notification",
+    listOrder: 0
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2018,
+    name: "Project Designation",
+    listOrder: 1
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2018,
+    name: "Initial Project Description",
+    listOrder: 2
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2018,
+    name: "Readiness Decision",
+    listOrder: 3
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2018,
+    name: "Issued Process Order",
+    listOrder: 4
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2018,
+    name: "Draft Process Order",
+    listOrder: 5
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2018,
+    name: "Draft EAC Application",
+    listOrder: 6
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2018,
+    name: "Revised EAC Application",
+    listOrder: 7
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2018,
+    name: "Draft Assessment Report",
+    listOrder: 8
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2018,
+    name: "Draft EAC Package",
+    listOrder: 9
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2018,
+    name: "Referrral Package",
+    listOrder: 10
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2018,
+    name: "Time Limit Extension",
+    listOrder: 11
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2018,
+    name: "Time Limit Imposition",
+    listOrder: 12
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2018,
+    name: "Assessment Suspension",
+    listOrder: 13
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2018,
+    name: "Withdrawal",
+    listOrder: 14
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2018,
+    name: "Termination",
+    listOrder: 15
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2018,
+    name: "Other",
+    listOrder: 16
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2018,
+    name: "Certificate Decision",
+    listOrder: 17
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2018,
+    name: "Certificate Extension",
+    listOrder: 18
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2018,
+    name: "Substancial Start Decision",
+    listOrder: 19
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2018,
+    name: "Compliance & Enforcement",
+    listOrder: 20
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2018,
+    name: "Post-Decision Materials",
+    listOrder: 21
+  },
+  {
+    type: "label",
+    _schemaName: "List",
+    legislation: 2018,
+    name: "Amendment",
+    listOrder: 22
+  },
+  
+];
