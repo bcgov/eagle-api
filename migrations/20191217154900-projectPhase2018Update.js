58e75a12f2c67e43517f9174b6ec81ddf8d9daad
Kathy Do <kdodph9@gmail.com>
2019-12-18 15:08:09 -0800

    EE-573 Drop down update for project phase on documents

---
+++ migrations/20191217154900-projectPhase2018Update.js
@@ -0,0 +1,85 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+let newItem = {
+  "type" : "projectPhase",
+  "_schemaName" : "List",
+  "name" : "Project Designation",
+  "legislation" : 2018,
+  "read" : [
+		"public",
+		"staff",
+		"sysadmin"
+	],
+	"write" : [
+		"staff",
+		"sysadmin"
+  ],
+  "listOrder" : 0
+};
+
+let docList_order = [1,2,3,4,5,6,14,7,8,9,10,11,16,12,13,15,17,0];
+
+exports.up = function(db) {
+    let mClient;
+    return db.connection.connect(db.connectionString, { native_parser: true })
+      .then((mClientInst) => {
+        // mClientInst is an instance of MongoClient
+        mClient = mClientInst;
+        var p = mClient.collection('epic');
+        
+        p.updateOne({"_schemaName":"List", "legislation": 2018, "name": "EA Readiness Decision"}, {$set: {"name": "Readiness Decision"}});
+        p.updateOne({"_schemaName":"List", "legislation": 2018, "name": "Application Development & Review"}, {$set: {"name": "Application Development and Review"}});
+        p.updateOne({"_schemaName":"List", "legislation": 2018, "name": "Post Decision"}, {$set: {"name": "Post Decision - Extension"}});
+
+        p.insertOne(newItem);
+
+
+        p.aggregate([
+          { $match: {_schemaName:"List", type: "projectPhase", legislation: 2018} }
+        ])
+          .toArray()
+          .then((arr) => {
+            let i = 0;
+            for (let item of arr) {
+              p.update(
+                { _id: item._id },
+                {
+                  $set: { listOrder: docList_order[i] }
+                }
+              )
+              i++;
+            }
+          }
+          ).catch((e) => {
+            console.log("error: ", e);
+            mClient.close()
+        });
+      })
+      .catch((e) => {
+        console.log("e:", e);
+        mClient.close()
+      });
+  };
+  
+
+exports.down = function(db) {
+  return null;
+};
+
+exports._meta = {
+  "version": 1
+};
