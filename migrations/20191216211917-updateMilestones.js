d6e37300f8a8949bb89ad364134d2654ccaa2d1c
Alastair Beaumont <a.beaumont11@gmail.com>
2019-12-19 14:35:41 -0800

    Update milestone (#325)
    * Adding in the new milestone lists
    
    * need to update the list name matching
    
    not matching on names with the old list right now
    
    * Updated the migration with the oldname fields
    
    * Updating to include the new milestone updates
    
    * Updating some oldName fields

---
+++ migrations/20191216211917-updateMilestones.js
@@ -0,0 +1,76 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+var mongoose = require('mongoose');
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+let migrationItems = require(process.cwd() + '/migrations_data/milestone_lists');
+
+exports.up = function(db) {
+  let mClient;
+  return db.connection.connect(db.connectionString, { native_parser: true})
+    .then((mClientInst) => {
+      mClient = mClientInst;
+      var p = mClient.collection('epic');
+      p.insert(migrationItems.newMilestones)
+      //Delete Revised Assessment Repo
+      p.aggregate([
+        { $match: {_schemaName:"List", type: "label"} }
+      ])
+      .toArray()
+      .then(function(arr) {
+        for (let item of arr) {
+          const milestoneName = item.name;
+          if (milestoneName === "Revised Assessment Report") {
+            //Delete this entry
+            p.deleteOne({_id: item._id});
+          }
+          const lookupMilestone = getMilestoneListObject(milestoneName);
+          if (lookupMilestone) {
+            p.update(
+              {
+                _id: item._id
+              },
+              {
+                $set: {
+                  listOrder: lookupMilestone.listOrder,
+                  name: lookupMilestone.name
+                }
+              }
+            )
+          }
+          }
+          mClient.close();
+      })
+      //Add in new milestone object
+    })
+    .catch((e) => {
+      console.log("e:", e);
+      mClient.close()
+    });
+};
+function getMilestoneListObject(milestoneName) {
+  for (var i=0; i < migrationItems.milestoneList.length; i++) { 
+    const milestoneItem = migrationItems.milestoneList[i];
+    if (milestoneItem.name === milestoneName || milestoneItem.oldName && milestoneItem.oldName === milestoneName) {
+      return milestoneItem;
+    }
+  }
+}
+exports.down = function(db) {
+  return null;
+};
+
+exports._meta = {
+  "version": 1
+};
