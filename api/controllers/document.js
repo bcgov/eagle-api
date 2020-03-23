d8c50d53eaca5b54cdd33ac409abaf14d3385694
Kit Armstrong <kitarmstrongbc@gmail.com>
2020-03-23 15:31:12 -0700

    add null check

--- api/controllers/document.js
+++ api/controllers/document.js
@@ -543,7 +543,8 @@ exports.protectedPost = async function (args, res) {
               doc.internalMime = upfile.mimetype;
 
               let formattedLeg = null;
-              if (typeof args.swagger.params.legislation.value === 'string' || typeof arguments.swagger.params.legislation.value === 'number') {
+              if ((args.swagger.params.legislation && typeof args.swagger.params.legislation.value === 'string') ||
+                  (args.swagger.params.legislation && typeof arguments.swagger.params.legislation.value === 'number')) {
                 formattedLeg = parseInt(args.swagger.params.legislation.value, 10);
               }
               doc.legislation = formattedLeg;
