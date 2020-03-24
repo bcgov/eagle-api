1727969a4dcd342facea5554aab6906a4b4d2a76
Mark Lisé <marklise@users.noreply.github.com>
2020-03-24 13:39:31 -0700

    CAC Updated work (#441)
    * EE-910: Automatic email service integration for subscriptions.
    EE-781: Unsubscribe users automatically
    Update logging, misc.
    NOBUG: Update link in email
    NOBUG: Fix models for EE-936
    
    * NOBUG: Fix UT runner

--- api/helpers/models/cacUser.js
+++ api/helpers/models/cacUser.js
@@ -1,11 +1,14 @@
 module.exports = require('../models')('CACUser', {
-  name            : { type: String, default:'' },
-  email           : { type: String },
-  liveNear        : { type: String, default: '' },
-  memberOf        : { type: String, default: '' },
-  knowledgeOf     : { type: String, default: '' },
-  additionalNotes : { type: String, default: '' },
-  project         : { type: 'ObjectId', ref: 'Project', default: null, index: true },
-  read            : [{ type: String, trim: true, default: 'sysadmin' }],
-  write           : [{ type: String, trim: true, default: 'sysadmin' }]
+  name              : { type: String, default:'' },
+  email             : { type: String },
+  liveNear          : { type: Boolean, default: false },
+  liveNearInput     : { type: String, default: '' },
+  memberOf          : { type: Boolean, default: false },
+  memberOfInput     : { type: String, default: '' },
+  knowledgeOf       : { type: Boolean, default: false },
+  knowledgeOfInput  : { type: String, default: '' },
+  additionalNotes   : { type: String, default: '' },
+  project           : { type: 'ObjectId', ref: 'Project', default: null, index: true },
+  read              : [{ type: String, trim: true, default: 'sysadmin' }],
+  write             : [{ type: String, trim: true, default: 'sysadmin' }]
 }, 'epic');
