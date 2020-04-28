5990d6fb292bed8b87e22483ed5f74dbe4bd4f28
Mark Lisé <marklise@users.noreply.github.com>
2020-04-28 13:16:57 -0700

    Update mongoose_options.js (#523)
    Tweak to try to keep mongodb connections fresh.

--- config/mongoose_options.js
+++ config/mongoose_options.js
@@ -12,6 +12,7 @@ const mongooseOptions = {
   poolSize: 10, // Maintain up to 10 socket connections
   // If not connected, return errors immediately rather than waiting for reconnect
   bufferMaxEntries: 0,
+  keepAlive: 1, // Attempt to keep the socket active.
   connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
   socketTimeoutMS: 45000 // Close sockets after 45 seconds of inactivity
 };
