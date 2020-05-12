32666f59beca6f389cc644b7d34ea11b44101f2a
a1astair <alastairbeaumont@protonmail.com>
2020-05-12 16:16:54 -0700

    Fixed Virus scanning env logic

--- api/helpers/utils.js
+++ api/helpers/utils.js
@@ -6,7 +6,7 @@ var clamav          = require('clamav.js');
 var _serviceHost    = process.env.CLAMAV_SERVICE_HOST || '127.0.0.1';
 var _servicePort    = process.env.CLAMAV_SERVICE_PORT || '3310';
 var MAX_LIMIT       = 1000;
-
+const defaultLog      = require('winston').loggers.get('default');
 var DEFAULT_PAGESIZE  = 100;
 
 exports.buildQuery = function (property, values, query) {
@@ -37,22 +37,19 @@ exports.avScan = function (buffer) {
     // Write your buffer
     bufferStream.end(buffer);
 
-    clamav.ping(_servicePort, _serviceHost, 1000, function(err) {
+    clamav.ping(_servicePort, _serviceHost, 1000, function (err) {
       if (err) {
-        console.log('ClamAV service: ' + _serviceHost + ':' + _servicePort + ' is not available['+err+']');
+        defaultLog.warn('ClamAV service: ' + _serviceHost + ':' + _servicePort + ' is not available[' + err + ']');
         resolve(false);
       } else {
-        console.log('ClamAV service is alive: ' + _serviceHost + ':' + _servicePort);
         clamav.createScanner(_servicePort, _serviceHost)
-          .scan(bufferStream, function(err, object, malicious) {
+          .scan(bufferStream, function (err, object, malicious) {
             if (err) {
-              console.log(err);
+              defaultLog.info('Error:', err);
               resolve(false);
             } else if (malicious) {
-              console.log('Malicious object FOUND');
               resolve(false);
             } else {
-              console.log('Virus scan OK');
               resolve(true);
             }
           });
