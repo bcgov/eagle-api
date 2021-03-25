736b31339fb2a28064627d263890c6d379293f89
Morgan Thompson <morgan.thompson@gov.bc.ca>
2021-03-25 12:17:43 -0700

    remove deprecated flags from mongoose config

--- config/mongoose_options.js
+++ config/mongoose_options.js
@@ -6,10 +6,8 @@
 const mongooseOptions = {
   useUnifiedTopology: true,
   useNewUrlParser: true,
+  useCreateIndex: true,
   allowDiskUse: true,
-  autoReconnect: true,
-  reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
-  reconnectInterval: 500, // Reconnect every 500ms
   poolSize: 10, // Maintain up to 10 socket connections
   // If not connected, return errors immediately rather than waiting for reconnect
   bufferMaxEntries: 0,
