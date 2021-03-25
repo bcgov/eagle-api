e9d79a80fe37c33476e67c11915cb9fc9744d96f
Morgan Thompson <morgan.thompson@gov.bc.ca>
2021-03-25 12:28:54 -0700

    fix use of allowDiskUse flag

--- config/mongoose_options.js
+++ config/mongoose_options.js
@@ -7,7 +7,6 @@ const mongooseOptions = {
   useUnifiedTopology: true,
   useNewUrlParser: true,
   useCreateIndex: true,
-  allowDiskUse: true,
   poolSize: 10, // Maintain up to 10 socket connections
   // If not connected, return errors immediately rather than waiting for reconnect
   bufferMaxEntries: 0,
