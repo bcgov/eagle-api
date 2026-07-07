037ffbbd39e9531f47a6239d007700bee4d2eaca
Daniel Truong <daniel@digitalspace.ca>
2026-07-07 20:18:05 +0000

    fix(migration): inline track project mappings

--- migrations/20260707000000-add-track-project-ids.js
+++ migrations/20260707000000-add-track-project-ids.js
@@ -1,21 +1,19 @@
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
 const { ObjectId } = require('mongodb');
+
+const MAPPINGS = [
+  { id: 129, epic_guid: '588510b0aaecd9001b8142a1' },
+  { id: 130, epic_guid: '588510b0aaecd9001b8142a2' },
+  { id: 131, epic_guid: '588510b0aaecd9001b8142a3' },
+  { id: 132, epic_guid: '588510b0aaecd9001b8142a4' }
+];
 
 module.exports = {
   async up(db, client) {
-    const mappingsPath = path.join(__dirname, '../migrations_data/track_project_mappings.json');
-    if (!fs.existsSync(mappingsPath)) {
-      console.log('Mappings file not found at:', mappingsPath);
-      return;
-    }
-
-    const mappings = JSON.parse(fs.readFileSync(mappingsPath, 'utf8'));
     const epicCollection = db.collection('epic');
 
-    for (const map of mappings) {
+    for (const map of MAPPINGS) {
       if (!map.epic_guid || !map.id) continue;
       
       try {
